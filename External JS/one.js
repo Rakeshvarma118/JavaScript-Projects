b2cc6ed27c7a613d89256d7bfa819573b1997930
rakeshvarmaa118 <rakeshvarmaa@gmail.com>
2025-10-19 18:59:41 +0530

    updated

---
+++ External JS/one.js
@@ -0,0 +1,29 @@
+//1.WITH PARAMETERS AND WITH RETURN STATAEMEENT
+        function circleArea1(r)
+        {
+            const PI=3.14
+            return PI*r*r
+        }
+
+        //2.WITHOUT PARAMETERS AND WITH RETURN STATAEMEENT
+        function circleArea2()
+        {
+            const PI=3.14
+            var r=5
+            return PI*r*r
+        }
+
+        //3.WITH PARAMETERS AND WITHOUT RETURN STATAEMEENT
+        function circleArea3(r)
+        {
+            const PI=3.14
+            console.log(PI*r*r)
+        }
+
+        //4.WITHOUT PARAMETERS AND WITHOUT RETURN STATAEMEENT
+        function circleArea4()
+        {
+            const PI=3.14
+            var r=5
+            console.log(PI*r*r)
+        }
